5b7f5fc9c65dde54b485c852318ce096b00bccf1
Blen <azblen1@gmail.com>
2018-10-03 13:13:42 -0400

    new feature

--- src/me/Blen/AccountPair.java
+++ src/me/Blen/AccountPair.java
@@ -3,9 +3,22 @@ package me.Blen;
 public class AccountPair {
         public int accountNum= 000000;
         public int pin = 0000;
-        public AccountPair(int myAccount, int myPin) {
+        private double balance;
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    public AccountPair(int myAccount, int myPin, double myBalance) {
             this.accountNum = myAccount;
             this.pin = myPin;
+            this.balance=myBalance;
         }
 
         public AccountPair(){
